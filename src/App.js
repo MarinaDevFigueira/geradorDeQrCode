043d79ff2603c227fe9108d6ee1c330478a07dbf
ilemuelfigueira <lfigueiradev@gmail.com>
2025-05-23 20:02:10 -0300

    Subindo arquivo central

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import React, { useState } from 'react';
+
+import './App.css';
+import QRcodeCanvas from './QRcodeCanvas';
+
+function App() {
+
+  const [text,setText]= useState("")
+  return (
+    <div className="App">
+      <h1>Gerador de QR Code</h1>
+      <input type="text" onChange={(element)=>setText(element.target.value)}  value={text} placeholder='www.google.com'/>
+      <QRcodeCanvas text={text}/>
+    </div>
+  );
+}
+
+export default App;
